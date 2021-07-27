6351692da63c91b17e626380dc9d7d4ecd6e0c84
mohammed599 <blurryface599@gmail.com>
2021-07-27 16:23:12 +0300

    Modified Blog

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const blog = document.querySelector(".blog");
 const btn = document.querySelector(".read-more");
+const btnLess = document.querySelector(".show-less");
 
 btn.addEventListener("click", (e) => {
   blog.style.overflow = "visible";
   btn.style.visibility = "hidden";
+  btnLess.visibility = "visible";
+});
+btnLess.addEventListener("click", (e) => {
+  blog.style.overflow = "hidden";
+  btn.style.visibility = "visible";
 });
